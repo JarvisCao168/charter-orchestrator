ec9e5b05bbdabf472c9d1634f3a7055db7e6f8e0
JarvisCao168 <63468969@qq.com>
2026-09-16 08:17:21 +0800

    v3.14: test_v3_13.py

--- tests/test_v3_13.py
+++ tests/test_v3_13.py
@@ -11,7 +11,7 @@ import charter
 
 
 def test_version_is_v3_13():
-    assert charter.__version__.startswith("3.13")
+    assert charter.__version__.startswith("3.14")
 
 
 def test_repair_and_rerun_exported():
